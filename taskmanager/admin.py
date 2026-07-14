cf89d90db8ab856938dc78aa4dc78380fda78782
Katya-Korotenko <korotenkok99943@gmail.com>
2026-07-14 21:22:13 +0200

    update

--- taskmanager/admin.py
+++ taskmanager/admin.py
@@ -5,12 +5,16 @@ from .models import Task, SubTask, Category
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
     list_display = ('title', 'status', 'deadline')
+    search_fields = ('title',)
+    list_filter = ('status',)
 
 @admin.register(SubTask)
 class SubTaskAdmin(admin.ModelAdmin):
     list_display = ('title', 'task' , 'status', 'deadline')
+    search_fields = ('title',)
+    list_filter = ('status',)
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-
     list_display = ('name',)
+    search_fields = ('name',)
--- taskmanager/models.py
+++ taskmanager/models.py
@@ -1,28 +1,28 @@
 from django.db import models
 
-STATUS_CHOICES = [
-('new', 'New'),
-('in progress', 'In progress'),
-('pending', 'Pending'),
-('blocked', 'Blocked'),
-('done', 'Done'),
-]
+class Status(models.TextChoices):
+    NEW = 'new', 'New'
+    IN_PROGRESS = 'in progress', 'In progress'
+    PENDING = 'pending', 'Pending'
+    BLOCKED = 'blocked', 'Blocked'
+    DONE = 'done', 'Done'
 
 class Category(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
 
     class Meta:
+        db_table = 'task_manager_category'
         verbose_name = 'Category'
         verbose_name_plural = 'Categories'
 
 class Task(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, unique=True)
     description = models.TextField()
     categories = models.ManyToManyField(Category, related_name='tasks', blank=True)
-    status = models.CharField(max_length=100, choices=STATUS_CHOICES)
+    status = models.CharField(max_length=100, choices=Status.choices, default=Status.NEW)
     deadline = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -30,18 +30,27 @@ class Task(models.Model):
         return self.title
 
     class Meta:
-        constraints = [models.UniqueConstraint(fields=['title', 'deadline'], name='unique_title_deadline')]
+        db_table = 'task_manager_task'
+        ordering = ['-created_at']
+        verbose_name = 'Task'
+        #стоит ли оставлять эту строчку, если в поле title есть unique
+        # constraints = [models.UniqueConstraint(fields=['title', 'deadline'], name='unique_title_deadline')]
 
 
 
 class SubTask(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, unique=True)
     description = models.TextField()
     task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='subtasks')
-    status = models.CharField(max_length=100, choices=STATUS_CHOICES)
+    status = models.CharField(max_length=100, choices=Status.choices, default=Status.NEW)
     deadline = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        db_table = 'task_manager_subtask'
+        verbose_name = 'SubTask'
+        ordering = ['-created_at']
 
