20e5279a41fc0373345abe45c0db97e4627cfc01
Katya-Korotenko <korotenkok99943@gmail.com>
2026-08-11 23:32:42 +0200

    upgreat

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -23,7 +23,7 @@ class Task(models.Model):
     description = models.TextField()
     categories = models.ManyToManyField(Category, related_name='tasks', blank=True)
     status = models.CharField(max_length=100, choices=Status.choices, default=Status.NEW)
-    deadline = models.DateField()
+    deadline = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
