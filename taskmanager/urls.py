2ff7af67b322d8bbceb4ea325e92fbae66d599ed
Katya-Korotenko <korotenkok99943@gmail.com>
2026-08-15 18:50:42 +0200

    SubTaskDetailUpdateDeleteView, SubTaskListCreateView

--- taskmanager/urls.py
+++ taskmanager/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import TaskCreateView, TaskListView, TaskDetailView, TaskStatsView
+from .views import TaskCreateView, TaskListView, TaskDetailView, TaskStatsView, SubTaskListCreateView, SubTaskDetailUpdateDeleteView
 
 urlpatterns = [
     path('tasks/create/', TaskCreateView.as_view(), name='task-create'),
     path('tasks/list/', TaskListView.as_view(), name='task-list'),
     path('tasks/detail/<int:pk>/', TaskDetailView.as_view(), name='task-detail'),
     path('tasks/stats/', TaskStatsView.as_view(), name='task-stats'),
+    path('subtask/list/', SubTaskListCreateView.as_view(), name='subtask-list'),
+
+    path('subtask/<int:subtask_id>/', SubTaskDetailUpdateDeleteView.as_view(), name='subtask-detail-update-delete'),
 ]
