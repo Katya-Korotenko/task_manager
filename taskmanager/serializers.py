7c7b3d63d0d99566b50d258a37e4f8a8c5df29a0
Katya-Korotenko <korotenkok99943@gmail.com>
2026-09-01 09:40:27 +0200

    UPD RegisterSerializer

--- taskmanager/serializers.py
+++ taskmanager/serializers.py
@@ -1,5 +1,9 @@
 from rest_framework import serializers
 from django.utils import timezone
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+
+
 from .models import Task, SubTask, Category
 
 
@@ -55,3 +59,21 @@ class TaskDetailSerializer(serializers.ModelSerializer):
         model = Task
         fields = ['id', 'title', 'owner', 'description', 'status', 'deadline', 'created_at', 'subtasks']
         read_only_fields = ['owner']
+
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, validators=[validate_password])
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("Email already in use")
+        return value
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
